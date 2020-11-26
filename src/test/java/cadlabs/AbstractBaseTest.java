a68105edbeb8e11bdaac53d2b9ad2f05b9d0903a
Hervé Paulino <herve.paulino@fct.unl.pt>
2020-11-26 11:49:04 +0000

    assert the correctness of the tests' results only when the return of expectedResult is not null

--- src/test/java/cadlabs/AbstractBaseTest.java
+++ src/test/java/cadlabs/AbstractBaseTest.java
@@ -24,24 +24,39 @@ public abstract class AbstractBaseTest<Input, Result> {
 		// comment (or change configuration) if you want the entire log
 		spark.sparkContext().setLogLevel("ERROR");
 	}
-	
-	
+
+
+	/**
+	 * Pre-processing of the input file to generate a dataset of type Input
+	 *
+	 * @param file The input file
+	 * @return The generated dataset
+	 */
 	protected abstract Input processInputFile(String file);
-	
-	protected abstract Result run(Input flights);
-	
+
+	/**
+	 * The test to execute over the input dataset (of type Input)
+	 *
+	 * @param dataset The input dataset
+	 * @return The result of the test
+	 */
+	protected abstract Result run(Input dataset);
+
+	/**
+	 * The result the test should produce.
+	 * If the method returns null, the output of the test is not tested.
+	 * @return
+	 */
 	protected abstract String expectedResult();
-	
-	
-	
+
+
 	@Test
 	public void run() {
 		
 		String file = "data/flights.csv";
 		
 		startSpark();
-		
-		
+
 		Input flights = processInputFile(file);
 
 		long start = System.currentTimeMillis();
@@ -49,8 +64,12 @@ public abstract class AbstractBaseTest<Input, Result> {
 		long elapsed = System.currentTimeMillis() - start;
 		
 		System.err.println("Elapsed Time: " + elapsed);
-		assertEquals(result.toString(), expectedResult());
-		
+
+		String expectedResult = expectedResult();
+		if (expectedResult != null) {
+			System.err.println("Asserting the correctness of the test's result");
+			assertEquals(result.toString(), expectedResult());
+		}
 		// terminate the session
 		spark.stop();
 	}
