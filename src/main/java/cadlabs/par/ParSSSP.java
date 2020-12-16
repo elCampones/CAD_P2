619a49a3e310c96c0db2b2fbf0822b01d32a8a1e
elCampones <p.campones@campus.fct.unl.pt>
2020-12-16 17:23:12 +0000

    Removed found array. No need as most paths need the minimal number hops

--- src/main/java/cadlabs/par/ParSSSP.java
+++ src/main/java/cadlabs/par/ParSSSP.java
@@ -17,7 +17,6 @@ public class ParSSSP extends AbstractFlightAnalyser<Path> {
 
     private final String srcName, destName;
 
-    //	private final IndexedRowMatrix graph;
     private final JavaRDD<IndexedRow> graph;
 
     public ParSSSP(String srcName, String destName, JavaRDD<Flight> flights) {
@@ -25,9 +24,6 @@ public class ParSSSP extends AbstractFlightAnalyser<Path> {
         this.srcName = srcName;
         this.destName = destName;
         graph = GraphBuilder.buildGraph(flights).rows().toJavaRDD();
-        //for (IndexedRow row : graph.take(2000))
-        //	System.out.println(row);
-
     }
 
     @Override
@@ -35,40 +31,27 @@ public class ParSSSP extends AbstractFlightAnalyser<Path> {
         graph.cache();
         int source = Flight.getAirportIdFromName(srcName);
         int destination = Flight.getAirportIdFromName(destName);
-        //int source = srcName, destination = destName;
         int nAirports = (int) Flight.getNumberAirports();
 
         Double[] shortestPath = new Double[nAirports];
         int[] from = new int[nAirports];
-        boolean[] confirmed = new boolean[nAirports];
         for (int i = 0; i < shortestPath.length; i++) {
             shortestPath[i] = Double.MAX_VALUE;
             from[i] = -1;
         }
-
         shortestPath[source] = .0;
-//        from[source] = source;
 
         Queue<Pair> toVisit = new PriorityQueue<>(nAirports);
         toVisit.add(new Pair(0, source));
-        int found;
-        int iterations = 0;
-        while (!toVisit.isEmpty() && (found = toVisit.peek().node) != destination){
 
-            if (confirmed[found]) {
-                toVisit.remove();
-                continue;
-            }
+        while (!toVisit.isEmpty()){
 
             Set<Long> frontierNodes = toVisit.stream().map(t -> (long) t.node)
-                    .filter(n -> !confirmed[Math.toIntExact(n)])                //TODO This might not be necessary. Confirmed nodes won't be reintroduced to the priority queue (I think)
                     .collect(Collectors.toSet());
-
             toVisit.clear();
 
             JavaRDD<IndexedRow> partitionRows =
                     graph.filter(s -> frontierNodes.contains(s.index()));
-            confirmed[found] = true;
 
             JavaPairRDD<Integer/*origin*/, Tuple2<Integer/*dest*/, Double/*newDistance*/>> modified =
                     partitionRows.flatMapToPair(
@@ -82,26 +65,22 @@ public class ParSSSP extends AbstractFlightAnalyser<Path> {
                                 }
                                 return l.iterator();
                             }
-                    ).filter(v -> !confirmed[v._2._1])
-                            .filter(v -> (v._2._2 < shortestPath[v._2._1]));
+                    ).filter(v -> (v._2._2 < shortestPath[v._2._1]));
 
             JavaPairRDD<Integer/*dest*/, Tuple2<Integer/*origin*/, Double/*newDistance*/>> minModifications =
                     modified.mapToPair(m -> new Tuple2<>(m._2._1, new Tuple2<>(m._1, m._2._2)))
                             .reduceByKey((v1, v2) -> (v1._2 < v2._2) ? v1 : v2);
 
-            List<Tuple2<Integer, Tuple2<Integer, Double>>> modifiedManifest = minModifications.collect();
-
-            /*
-            * For this reduction locks aren't necessary because the previous lambda expression over the RDDs assures that there is only one modification per destination node
-             */
-            modifiedManifest.forEach(m -> {
+            minModifications.collect().forEach(m -> {
                 if (m._2._2 < shortestPath[m._1]) {
                     shortestPath[m._1] = m._2._2;
                     from[m._1] = m._2._1;
                     toVisit.add(new Pair(m._2._2, m._1));
                 }
             });
+
         }
+
 
         return new Path(source, destination, from, Arrays.stream(shortestPath).mapToDouble(d -> d).toArray());
     }
